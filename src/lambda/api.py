24d0e3f7ae544df1174a96aca32de8de3a6439a1
John Byrd <john.byrd@sonance.com>
2025-06-07 23:28:10 -0700

    Try the correctly syntaxed catch-all route

--- src/lambda/api.py
+++ src/lambda/api.py
@@ -184,7 +184,7 @@ def get_apikey_status(user_id):
 
 # --- Catch-all for unsupported endpoints ---
 
-@app.route(f"/{API_VER}/{{proxy+}}", method=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"])
+@app.route(f"/{API_VER}/.+}", method=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"])
 def not_implemented():
     return SafeResponse(status_code=404, message=f"Endpoint '{app.current_event.path}' not implemented.")
 
